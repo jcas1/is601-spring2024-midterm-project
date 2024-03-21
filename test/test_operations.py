a87dcc1272b9733e0cb113652216890239c6471e
jcas1 <johncastillo412@gmail.com>
2024-03-20 23:57:09 -0400

    added test for the operations

---
+++ test/test_operations.py
@@ -0,0 +1,24 @@
+'''Testing General Operations'''
+import pytest
+from calculator.operations import add, subtract, multiply, divide
+
+def test_add():
+    '''test for addition'''
+    assert add(4,5) == 9
+
+def test_subtract():
+    '''test for subtraction'''
+    assert subtract(4,5) == -1
+
+def test_multiply():
+    '''test for multiplication'''
+    assert multiply(4,5) == 20
+
+def test_divide():
+    '''test for division'''
+    assert divide(4,5) == 0.8
+
+def test_divide_by_zero():
+    '''test for division by zero'''
+    with pytest.raises(ValueError):
+        divide(4,0)
