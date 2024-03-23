5fe8ce6d43b92d064fbd62b1321919e2a1562238
jcas1 <johncastillo412@gmail.com>
2024-03-23 00:31:27 -0400

    updated to include calculator class

---
+++ calculator/__init__.py
@@ -0,0 +1,48 @@
+'''Calculator class that performs the calculations and stores them'''
+from calculator.calculation import Calculation
+from calculator.calc_history import CalcHistory
+from calculator.operations import add, subtract, multiply, divide
+
+class Calculator:
+    '''Calculator class'''
+    @staticmethod
+    def calculate(x, y, operation):
+        '''
+        takes two operands, and one operation
+        will perform the calculation and save it as well
+        '''
+        calc = Calculation(x, y, operation)
+        CalcHistory.add_calc(calc)
+        return calc.perform()
+
+    @staticmethod
+    def add(x, y):
+        '''
+        Utilized the calculate method to specifically add two numbers
+        and store the calculation
+        '''
+        return Calculator.calculate(x, y, add)
+
+    @staticmethod
+    def subtract(x, y):
+        '''
+        Utilized the calculate method to specifically subtract two numbers
+        and store the calculation
+        '''
+        return Calculator.calculate(x, y, subtract)
+
+    @staticmethod
+    def multiply(x, y):
+        '''
+        Utilized the calculate method to specifically multiply two numbers
+        and store the calculation
+        '''
+        return Calculator.calculate(x, y, multiply)
+
+    @staticmethod
+    def divide(x, y):
+        '''
+        Utilized the calculate method to specifically divide two numbers
+        and store the calculation
+        '''
+        return Calculator.calculate(x, y, divide)
