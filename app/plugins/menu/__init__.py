f222821638862d138f2742ddf102545c97cdf9a4
jcas1 <johncastillo412@gmail.com>
2024-03-25 20:50:20 -0400

    menu command, still have to update

--- app/plugins/menu/__init__.py
+++
@@ -1,9 +0,0 @@
-'''Multiply Command'''
-# pylint: disable=missing-class-docstring
-from app.command_handler import Command
-
-class MenuCommand(Command):
-    def execute(self, *args):
-        print("Available plugins:")
-        for plugin_name in args:
-            print(f"- {plugin_name}")
