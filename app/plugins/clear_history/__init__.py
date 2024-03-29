5bc2a5a3ce6274dfef449ca6dba59dd13920648e
jcas1 <johncastillo412@gmail.com>
2024-03-29 16:02:02 -0400

    changes

--- app/plugins/clear_history/__init__.py
+++ app/plugins/clear_history/__init__.py
@@ -1,4 +1,4 @@
-'''Get Latest Calculation from History'''
+'''Clears History from command_history.csv'''
 # pylint: disable=missing-class-docstring
 from calculator.calc_history import CalcHistory
 from app.command_handler import Command
